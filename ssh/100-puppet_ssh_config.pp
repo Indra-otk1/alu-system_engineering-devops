80e3c44eabb669a088e6e69bd7cc4d74a819edeb
Indra-otk1 <e.singizwa@alustudent.com>
2026-02-23 23:13:45 +0200

    3rd commit

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,19 @@
-file { '/home/vagrant/.ssh/config':
-  ensure => 'present',
+# Using puppet
+# 1. Your SSH client configuration must be configured to use the private key ~/.ssh/school
+# 2. Your SSH client configuration must be configured to refuse to authenticate using a password
+
+include stdlib
+
+file_line { 'Refuse auth with password':
+  ensure => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Turn off passwd auth':
-  path  => '/home/vagrant/.ssh/config',
-  line  => '    PasswordAuthentication no',
-  match => 'PasswordAuthentication',
-}
-
-file_line { 'Declare identity file':
-  path  => '/home/vagrant/.ssh/config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => 'IdentityFile',
+file_line { 'specify identity file path':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
